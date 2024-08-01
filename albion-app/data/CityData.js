577761e53a753f4b12c2c5445f0098c5e74c91d7
Majed-Habli <majedhabli2020@gmail.com>
2024-08-02 02:20:38 +0300

    Added the crest source to each city.

--- albion-app/data/CityData.js
+++ albion-app/data/CityData.js
@@ -3,30 +3,35 @@
 export const imageData = [
   {
     source: require("@/assets/images/BridgewatchBanner.jpeg"),
+    crest: require("@/assets/images/crests/BridgewatchCrest.png"),
     title: "BridgeWatch",
     description: "Description for Image 1",
     resources: [0, 1, 2]
   },
   {
     source: require("@/assets/images/LymhurstBanner.jpeg"),
+    crest: require("@/assets/images/crests/LymhurstCrest.png"),
     title: "Lymhurst",
     description: "Description for Image 2",
     resources: [0, 1, 3]
   },
   {
     source: require("@/assets/images/MortlockBanner.jpeg"),
+    crest: require("@/assets/images/crests/MortlockCrest.png"),
     title: "MortLock",
     description: "Description for Image 3",
     resources: [0, 1, 3]
   },
   {
     source: require("@/assets/images/CarleonBanner.jpg"),
+    crest: require("@/assets/images/crests/CarleonCrest.png"),
     title: "Carleon",
     description: "Description for Image 4",
     resources: [0, 1, 2, 3]
   },
   {
     source: require("@/assets/images/FortsterlingBanner.jpg"),
+    crest: require("@/assets/images/crests/FortSterlingCrest.png"),
     title: "Fortsterling",
     description: "Description for Image 5",
     resources: [3, 1, 2]
