62dab742fec34bee7b06049b61872c6b25209040
Majed-Habli <majedhabli2020@gmail.com>
2024-08-02 01:28:31 +0300

    Updated the city data file.

--- albion-app/data/CityData.js
+++ albion-app/data/CityData.js
@@ -5,30 +5,42 @@ export const imageData = [
     source: require("@/assets/images/BridgewatchBanner.jpeg"),
     title: "BridgeWatch",
     description: "Description for Image 1",
+    resources: [0, 1, 2]
   },
   {
     source: require("@/assets/images/LymhurstBanner.jpeg"),
     title: "Lymhurst",
     description: "Description for Image 2",
+    resources: [0, 1, 3]
   },
   {
     source: require("@/assets/images/MortlockBanner.jpeg"),
     title: "MortLock",
     description: "Description for Image 3",
+    resources: [0, 1, 3]
   },
   {
     source: require("@/assets/images/CarleonBanner.jpg"),
     title: "Carleon",
     description: "Description for Image 4",
+    resources: [0, 1, 2, 3]
   },
   {
     source: require("@/assets/images/FortsterlingBanner.jpg"),
     title: "Fortsterling",
     description: "Description for Image 5",
+    resources: [3, 1, 2]
   },
   {
     source: require("@/assets/images/BercilienBanner.jpeg"),
     title: "Bercilien",
     description: "Description for Image 6",
   },
+];
+
+export const resources = [
+  "Hide",
+  "Cotton",
+  "Ore",
+  "Wood"
 ];
