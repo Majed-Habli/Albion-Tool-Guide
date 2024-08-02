c36048bad2b95bb69b86e2098cc4ee66051d8710
Majed-Habli <majedhabli2020@gmail.com>
2024-08-02 19:00:39 +0300

    Added refining bonuses to city data set.

--- albion-app/data/CityData.js
+++ albion-app/data/CityData.js
@@ -6,35 +6,76 @@ export const imageData = [
     crest: require("@/assets/images/crests/BridgewatchCrest.png"),
     title: "BridgeWatch",
     description: "Description for Image 1",
-    resources: [0, 1, 2]
+    resources: [0, 1, 2],
+    farming_yield_bonus: {
+      one : 'Beans',
+      two : 'Goats Milk',
+      three : 'Dragon Teasel',
+      four : 'Bundel of Corn'
+    }
   },
   {
     source: require("@/assets/images/LymhurstBanner.jpeg"),
     crest: require("@/assets/images/crests/LymhurstCrest.png"),
     title: "Lymhurst",
     description: "Description for Image 2",
-    resources: [0, 1, 3]
+    resources: [0, 1, 3],
+    farming_yield_bonus: {
+      one : 'Carrot',
+      two : 'Crenellated Burdock ',
+      three : 'Goose Eggs',
+      four : 'Pumpkin'
+    }
+  },
+  {
+    source: require("@/assets/images/ThetfortBanner.jpg"),
+    crest: require("@/assets/images/crests/ThetfortCrest.png"),
+    title: "Thetfort",
+    description: "Description for Image 2",
+    resources: [0, 1, 3],
+    farming_yield_bonus: {
+      one : 'Arcane Agaric',
+      two : 'Cabbage',
+      three : 'Firetouched Mullein',
+    }
   },
   {
     source: require("@/assets/images/MortlockBanner.jpeg"),
     crest: require("@/assets/images/crests/MortlockCrest.png"),
     title: "MortLock",
     description: "Description for Image 3",
-    resources: [0, 1, 3]
+    resources: [0, 1, 3],
+    farming_yield_bonus: {
+      one : 'Sheaf of Wheat',
+      two : 'Potatoes',
+      three : 'Elusive Foxglove',
+      four : 'Cows Milk'
+    }
   },
   {
     source: require("@/assets/images/CarleonBanner.jpg"),
     crest: require("@/assets/images/crests/CarleonCrest.png"),
     title: "Carleon",
     description: "Description for Image 4",
-    resources: [0, 1, 2, 3]
+    resources: [0, 1, 2, 3],
+    farming_yield_bonus: {
+      one : 'Brightleaf Comfrey',
+      two : 'Dragon Teasel',
+      three : 'Firetouched Mullein',
+    }
   },
   {
     source: require("@/assets/images/FortsterlingBanner.jpg"),
     crest: require("@/assets/images/crests/FortSterlingCrest.png"),
     title: "Fortsterling",
     description: "Description for Image 5",
-    resources: [3, 1, 2]
+    resources: [3, 1, 2],
+    farming_yield_bonus: {
+      one : 'Beans',
+      two : 'Goats Milk',
+      three : 'Dragon Teasel',
+      four : 'Bundel of Corn'
+    }
   },
   {
     source: require("@/assets/images/BercilienBanner.jpeg"),
